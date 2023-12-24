8c36222e9a60a68d7f76693ea9d71b859cc72504
Milan <m26030820@gmail.com>
2023-12-24 14:37:28 +0100

    JSX attributes and styling react elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 
+
 const fName = "Milan ";
 const lName = "Shubaev";
 const randomNumber = 1;
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
 
 function App() {
   return (
-    <div className="App">
-      <h1>My name is {fName + lName}</h1>
+    <div className='App'>
+      <h1 className='myName'>My name is {fName + lName}</h1>
       <p>My lucky number is {randomNumber}</p>
       <p>My random number is {Math.floor(Math.random()*10)}</p>
+
+      <p>Created by {fName}.</p>
+      <p>Copyright {currentYear}.</p>
+
+      <div>
+        <img className='food-img'src="https://as1.ftcdn.net/v2/jpg/05/89/66/36/1000_F_589663676_k1GF4cly0gOBWJbG5Z7JqFBzkrXWvnyw.jpg" alt="sushi stuff"/>
+        <img className='food-img' src= "https://as1.ftcdn.net/v2/jpg/03/32/16/52/1000_F_332165231_B81gcGf8AMRKMB0MjZZnqWMLbaF2JaVL.jpg" alt="more sushi stuff"/>
+        <img className='food-img'src="https://as1.ftcdn.net/v2/jpg/04/18/26/02/1000_F_418260218_MWxbtk2TEthdLvIzarWU4NMcPELIeJTq.jpg" alt="even more sushi stuff"/>
+      </div>
     </div>
   );
 }
