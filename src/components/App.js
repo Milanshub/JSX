ea3b307f0eeaf429dbe26701cce780f2190b1ab2
Milan <m26030820@gmail.com>
2023-12-26 17:07:13 +0100

    learning import and export ES6

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,27 @@ import '../App.css';
 import Greetings from './Greeting';
 import Heading from './Heading';
 import List from './List';
-
+import pi, { doublePi, triplePi } from './Math';
+import * as calc from './Calculator';
 
 function App() {
   return (
     <div className='App'>
+
+      <ul>
+        <li>{pi}</li>
+        <li>{doublePi()}</li>
+        <li>{triplePi()}</li>
+      </ul>
+
+      <ul>
+        <li>{calc.add(1, 2)}</li>
+        <li>{calc.multiply(2, 3)}</li>
+        <li>{calc.subtract(7, 2)}</li>
+        <li>{calc.divide(5, 2)}</li>
+      </ul>
+
+
       <Greetings />
       <Heading />
       <List />
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-// import express from "express";
-// import bodyParser from "body-parser";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-// const app = express();
-// app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,11 +15,4 @@ root.render(
 );
 
 
-// app.listen(3000, function (){
-//   console.log("The server has starter on port 3000");
-// });
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
