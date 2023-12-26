d0b9ea0f2cc4e74a5b995736870a540cc1c83d31
Milan <m26030820@gmail.com>
2023-12-26 11:55:41 +0100

    more components learning

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import Greetings from './Greeting';
+import Heading from './Heading';
+import List from './List';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Greetings />
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+
+
+export default App;
